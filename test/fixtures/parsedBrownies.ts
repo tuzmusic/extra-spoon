9e15126a7c02369b92bc7a84eaa6363f8401bb0d
Jonathan Tuzman <tuzmusic@gmail.com>
2020-11-27 18:20:36 -0500

    Remove some unneeded content. Update README.

---
+++ test/fixtures/parsedBrownies.ts
@@ -0,0 +1,141 @@
+/*
+import { RecipeJSON } from "../../src/typings/recipe.types";
+
+const parsedBrownies: RecipeJSON = {
+  title: "Fudgy Brownies {Think: Homemade Brownies Like The Boxed Mix!}",
+  instructions: [
+    {
+      text: "Position a rack in the lower third of the oven and preheat the oven to 325°F. Line the bottom and sides" +
+        " of" +
+        " an 8×8-inch square baking pan with parchment paper or foil, leaving an overhang on two opposite sides. Lightly grease with cooking spray and set aside.",
+      ingredients: [],
+      timers: []
+    },
+    {
+      text: "Combine the butter, sugar, cocoa, and salt in a medium microwave-wafe bowl. Microwave for one minute" +
+        " intervals, stirring in between, until the butter is melted and the mixture is smooth. Set the mixture aside until it cools slightly (is warm to the touch not hot). It will look gritty but it will become smooth once the other ingredients are added.Stir in the vanilla with a wooden spoon or rubber spatula.",
+      "ingredients": [
+        {
+          id: 2050,
+          name: "vanilla",
+        },
+        {
+          id: 1001,
+          name: "butter",
+        },
+        {
+          id: 19165,
+          name: "cocoa powder",
+        },
+        {
+          id: 19335,
+          name: "sugar",
+        },
+        {
+          id: 2047,
+          name: "salt",
+        }
+      ],
+      timers: [{
+        label: "",
+        durationSec: 60
+      }]
+    },
+    {
+      text: "Add the eggs one at a time, stirring vigorously after each one. When the batter looks thick, shiny, and" +
+        " well blended, add the flour and stir until you cannot see it any longer, then beat vigorously for 40 strokes with the wooden spoon or a rubber spatula.",
+      "ingredients": [
+        {
+          id: 20081,
+          name: "all purpose flour",
+        },
+        {
+          id: 1123,
+          name: "egg",
+        }
+      ],
+      timers: []
+    },
+    {
+      text: "Spread evenly in the lined pan.",
+      ingredients: [],
+      timers: []
+    },
+    {
+      text: "Bake until a toothpick inserted into the center emerges slightly moist with batter, 20 to 25 minutes.",
+      ingredients: [],
+      timers: [
+        {
+          label: "",
+          durationSec: 20 * 60
+        }
+      ]
+    },
+    {
+      text: "Let cool completely on a rack. After the brownies are cooled completely, I like to throw the pan in the" +
+        " refrigerator - cold brownies cut much easier and taste heavenly. Lift up the ends of the parchment or foil liner, and transfer the brownies to a cutting board.",
+      ingredients: [],
+      timers: []
+    },
+    {
+      text: "Cut into squares and serve.",
+      ingredients: [],
+      timers: []
+    }
+  ],
+  ingredients: [
+    {
+      id: 1001,
+      name: "butter",
+      originalText: "10 tablespoons (1 1/4 sticks or 5 ounces) butter",
+      amount: 10.0,
+      unit: "tablespoons",
+    },
+    {
+      id: 1123,
+      name: "eggs",
+      originalText: "2 large eggs",
+      amount: 2.0,
+      unit: "large",
+    },
+    {
+      id: 20081,
+      name: "flour",
+      originalText: "1/2 cup (2 3/8 ounces) all-purpose flour",
+      amount: 2.375,
+      unit: "ounces",
+    },
+    {
+      id: 2047,
+      name: "salt",
+      originalText: "1/2 teaspoon salt",
+      amount: 0.5,
+      unit: "teaspoon",
+    },
+    {
+      id: 19335,
+      name: "sugar",
+      originalText: "1 1/4 cups (9 7/8 ounces) sugar",
+      amount: 9.875,
+      unit: "ounces",
+    },
+    {
+      id: 19165,
+      name: "unsweetened cocoa powder",
+      originalText: "3/4 cup plus 2 tablespoons (2 7/8 ounces) unsweetened cocoa powder (natural or Dutch-process)",
+      amount: 0.75,
+      unit: "cup",
+    },
+    {
+      id: 2050,
+      name: "vanilla extract",
+      originalText: "1/2 teaspoon pure vanilla extract",
+      amount: 0.5,
+      unit: "teaspoon",
+    }
+  ],
+  
+};
+
+export default parsedBrownies;
+*/
